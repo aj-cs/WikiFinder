a33d05f9dd6d041b539b38ec83deca51e9dbc5f3
Arjun <128473547+ArjunHcs@users.noreply.github.com>
2025-02-10 10:56:27 +0100

    Updated hash 'function' to be case insensitive, updated functionality to follow C# schematics.

--- Index1.cs
+++ Index1.cs
@@ -1,132 +1,151 @@
 using System;
 using System.IO;
-namespace SearchEngineProject;
-
-internal class Index1
+namespace SearchEngineProject
 {
-    private WikiItem start;
-
-    private class DocumentItem
+    internal class Index1
     {
-        public string Title;
-        public DocumentItem? Next;
+        private const int TSize = 11831; // Use a prime num. 11831 
+        private WikiItem[] table;
 
-        public DocumentItem(string title, DocumentItem next)
+        private class WikiItem
         {
-            Title = title;
-            Next = next;
-        }
-
-    }
-    private class WikiItem
-    {
-        public string Str;
-        public DocumentItem DocumentList;
-        public WikiItem? Next;
-
-        public WikiItem(string s, WikiItem n)
-        {
-            Str = s;
-            DocumentList = null;
-            Next = n;
-        }
-    }
-
-    public Index1(string filename)
-    {
-        try
-        {
-            using (StreamReader input = new StreamReader(filename, System.Text.Encoding.UTF8))
+            public string Str;
+            public DocumentLog Log;
+            public WikiItem Next;
+            public WikiItem(string s, DocumentLog l, WikiItem n)
             {
-                string line;
-                string currentTitle = null;
-                start = null;
+                Str = s;
+                Log = l;
+                Next = n;
+            }
 
-                while ((line = input.ReadLine()) != null)
+        }
+
+        private class DocumentLog
+        {
+            public string Title;
+            public DocumentLog Next;
+            public DocumentLog(string title, DocumentLog next)
+            {
+                Title = title;
+                Next = next;
+            }
+        }
+
+        public Index1(string filename)
+        {
+            table = new WikiItem[TSize];
+            try
+            {
+                using (StreamReader input = new StreamReader(filename, System.Text.Encoding.UTF8))
                 {
-                    if (start == null || line == "---END.OF.DOCUMENT---")
+                    string line;
+                    string currentTitle = "";
+                    bool titleRead = false;
+                    while ((line = input.ReadLine()) != null)
                     {
-                        currentTitle = input.ReadLine();
-                        if (currentTitle == null)
+                        if (string.IsNullOrWhiteSpace(line))
+                            continue;
+                        if (line.Equals("---END.OF.DOCUMENT---"))
                         {
-                            break;
+                            titleRead = false;
+                            currentTitle = "";
+                            continue;
+                        }
+                        if (!titleRead)
+                        {
+
+                            currentTitle = line;
+                            Console.WriteLine(currentTitle);
+                            titleRead = true;
+                        }
+                        else
+                        {
+                            string[] words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                            foreach (string word in words)
+                            {
+                                InsertWord(word, currentTitle);
+                            }
                         }
                     }
-                    string[] words = line.Split(' ');
-                    foreach (string word in words)
-                    {
-                        Console.WriteLine(word);
-                        WikiItem node = FindOrCreateWord(word);
-                        AddDocumentToWord(node, currentTitle);
-                    }
                 }
             }
-        }
-        catch (FileNotFoundException)
-        {
-            Console.WriteLine("Error reading file " + filename);
-        }
-    }
-
-    private void AddDocumentToWord(WikiItem node, string title)
-    {
-        DocumentItem doc = node.DocumentList;
-        while (doc != null)
-        {
-            if (doc.Title.Equals(title, StringComparison.OrdinalIgnoreCase))
+            catch (FileNotFoundException)
             {
-                return;
+                Console.WriteLine("Error reading file " + filename);
             }
-            doc = doc.Next;
         }
-        node.DocumentList = new DocumentItem(title, node.DocumentList);
-    }
 
-    private WikiItem FindOrCreateWord(string word)
-    {
-        WikiItem current = start;
-        WikiItem previous = null;
-
-        while (current != null)
+        private void InsertWord(string word, string title)
         {
-            if (current.Str.Equals(word, StringComparison.OrdinalIgnoreCase))
+            word = word.ToLower();
+            int index = Math.Abs(word.GetHashCode()) % TSize; // we could implement our own hash function
+            WikiItem current = table[index];
+
+            while (current != null)
             {
-                return current;
-            }
-
-            previous = current;
-            current = current.Next;
-        }
-
-        WikiItem newWord = new WikiItem(word, null);
-        if (previous == null)
-        {
-            start = newWord;
-        }
-        else
-        {
-            previous.Next = newWord;
-        }
-        return newWord;
-    }
-
-    public void Search(string searchStr)
-    {
-        WikiItem current = start;
-        while (current != null)
-        {
-            if (current.Str.Equals(searchStr, StringComparison.OrdinalIgnoreCase))
-            {
-                Console.WriteLine($"The search string '{searchStr}' appears in: ");
-                DocumentItem doc = current.DocumentList;
-                while (doc != null)
+                if (current.Str.Equals(word))
                 {
-                    Console.WriteLine($"- {doc.Title}");
-                    doc = doc.Next;
+                    if (!DocExists(current.Log, title))
+                    {
+                        current.Log = new DocumentLog(title, current.Log);
+                    }
+                    return;
                 }
+
+                current = current.Next;
             }
-            current = current.Next;
+            // in case no word exists within the table yet
+            table[index] = new WikiItem(word, new DocumentLog(title, null), table[index]);
+
         }
-        Console.WriteLine($"'{searchStr}' could not be found in any document");
+
+        private WikiItem FindWord(string word)
+        {
+            word = word.ToLower();
+            int index = Math.Abs(word.GetHashCode()) % TSize; // case sensitive atm 
+            WikiItem current = table[index];
+            while (current != null)
+            {
+                if (current.Str.Equals(word))
+                    return current;
+                current = current.Next;
+            }
+            return null;
+        }
+
+        private bool DocExists(DocumentLog log, string title)
+        {
+            while (log != null)
+            {
+                if (log.Title.Equals(title))
+                    return true;
+                log = log.Next;
+            }
+            return false;
+        }
+
+        public bool Search(string searchStr)
+        {
+            WikiItem current = FindWord(searchStr);
+            if (current != null)
+            {
+                Console.WriteLine("Found " + searchStr + " in:");
+                DocumentLog log = current.Log;
+                while (log != null)
+                {
+                    Console.WriteLine(log.Title);
+                    log = log.Next;
+                }
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("No matches for " + searchStr + " found");
+                return false;
+            }
+        }
     }
 }
+
+
